e9755cfc332a4a1396acc77bace2c66137ce369d
juuroudojo <105825252+juuroudojo@users.noreply.github.com>
2023-10-30 00:54:19 +0200

    v0.01

--- src/Marketplace.sol
+++ src/Marketplace.sol
@@ -4,6 +4,14 @@ pragma solidity ^0.8.13;
 
 contract Marketplace {
     constructor() {
+
+    }
+
+    function buyTokenUSDT() {
+
+    }
+
+    function listToken() {
         
     }
 }
--- src/Token.sol
+++ src/Token.sol
@@ -1,9 +1,20 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Token is ERC20 {
+contract TestToken is ERC20, AccessControl {
+    constructor(uint256 initialSupply) ERC20("JoshRental", "JRT") {
+        _mint(msg.sender, initialSupply);
+    }
 
+    function mint(address to, uint256 amount) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _burn(from, amount);
+    }
 }
